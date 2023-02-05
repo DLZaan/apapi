8a0e043a4b6b447c7b41cb9e9b1b92aac4ab2fc2
Miłosz Kierepka <milosz.kierepka@gmail.com>
2023-02-05 22:11:23 +0100

    finalize v0.4.0 release

--- apapi/__version__.py
+++ apapi/__version__.py
@@ -1,8 +1,8 @@
 __title__ = "apapi"
 __description__ = "Unofficial Anaplan API Python client library"
 __url__ = "https://github.com/DLZaan/apapi"
-__version__ = "0.3.0"
+__version__ = "0.4.0"  # also needs to be bumped in docs/module.html.jinja2
 __author__ = "Milosz Kierepka"
 __author_email__ = "milosz.kierepka+work@gmail.com"
 __license__ = "Apache 2.0"
-__copyright__ = "Copyright 2022 Milosz Kierepka"
+__copyright__ = "Copyright 2022-2023 Milosz Kierepka"
